2e979dead4c86ceec41cbccb3a12bbe450b37f89
Jakub Janczyk <kubex321@gmail.com>
2020-11-20 18:56:59 +0100

    demo: 03-async

--- exercises/03-async.spec.js
+++ exercises/03-async.spec.js
@@ -1,17 +1,36 @@
 const { InMemoryRepository } = require('../src/InMemoryRepository');
 const { ProductsService } = require('../src/ProductsService');
 
-it.skip('should return empty list if no products',  () => {
+it('should return empty list if no products', async () => {
     // DEMO
+    const productsService = new ProductsService();
+
+    const products = await productsService.products();
+
+    expect(products).toEqual([]);
 });
 
-it.skip('should allow to add a product',  () => {
+it('should allow to add a product', async () => {
     // DEMO
+    const product = { id: '1', count: 1, price: 12 };
+    const productsService = new ProductsService();
+
+    await productsService.addProduct(product);
+
+    const allProducts = await productsService.products();
+    expect(allProducts).toContain(product);
 });
 
-it.skip('should not allow to add a product with no count', () => {
+it('should not allow to add a product with no count', async () => {
     // DEMO
+    const product = { id: '1', price: 12 };
+    const productsService = new ProductsService();
+
+    const addProductsPromise = productsService.addProduct(product);
+
+    await expect(addProductsPromise).rejects.toThrowError('Count cannot be empty or less than 0');
 });
+
 
 
 // TODO: Napisz test, który w którym dodasz dwa produkty i sprawdzisz, że oba są pobrane po wywołaniu metody products()
