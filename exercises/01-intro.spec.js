b75bfbb2ebac14d0df65200446b212546c45d9cd
Jakub Janczyk <kubex321@gmail.com>
2020-11-20 18:21:29 +0100

    finished: 01-intro

--- exercises/01-intro.spec.js
+++ exercises/01-intro.spec.js
@@ -3,6 +3,33 @@ it('example test should pass', () => {
 });
 
 // DEMO
+it('should add two numbers', () => {
+    // Arrange
+
+    // Act
+    const sum = add(1, 2);
+
+    // Assert
+    expect(sum).toEqual(3);
+});
+
+it('should subtract two numbers', () => {
+    // Given
+
+    // When
+    const result = subtract(3, 1);
+
+    // Then
+    expect(result).toBe(2);
+});
+
+it('should add element to an array', () => {
+    const array = [1, 2];
+
+    array.push(3);
+
+    expect(array).toContain(3);
+});
 
 
 function add(a, b) {
