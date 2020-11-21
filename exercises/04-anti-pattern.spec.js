d59f10fa5b34c4f15a0c29b03cec7438c55746d6
Jakub Janczyk <kubex321@gmail.com>
2020-11-21 01:56:48 +0100

    finished: 04-anti-pattern

--- exercises/04-anti-pattern.spec.js
+++ exercises/04-anti-pattern.spec.js
@@ -1,8 +1,28 @@
 const { pricesCalculator } = require('../src/price-calculator');
 const { ProductsService } = require('../src/ProductsService');
 
-it.skip('should calculate price for all products in stock',  () => {
-    // DEMO
+const mockTotalPrice = jest.fn();
+
+jest.mock('../src/price-calculator', () => {
+    return {
+        pricesCalculator: jest.fn(() => {
+            return {
+                totalPriceForProducts: mockTotalPrice
+            }
+        })
+    }
+});
+
+it('should calculate price for all products in stock',  async () => {
+    mockTotalPrice.mockReturnValue(125);
+    const productsService = {
+        products: jest.fn().mockResolvedValue([{id: '1', price: 125, count: 1}])
+    };
+
+    const totalPrice = await calculateTotalPriceForAllProductsInStock(productsService);
+
+    expect(totalPrice).toEqual(125);
+    expect(mockTotalPrice).toHaveBeenCalledWith([{id: '1', price: 125, count: 1}]);
 });
 
 const calculateTotalPriceForAllProductsInStock = async (service) => {
